71a0241176d6fb24b65cf1390f54352f5f374055
Jens Lindner <jens.lindner@prodyna.com>
2021-05-06 14:15:37 +0200

    change name :)

--- snippets/camunda-tasklist-examples/camunda-angular-app/src/app/app.component.ts
+++ snippets/camunda-tasklist-examples/camunda-angular-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Camunda Angular Tasklist';
+  title = 'Angular Tasklist Example';
 }
